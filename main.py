ef9928dd616170defe4fff4485f6277fac023ccc
Martin Strandgren <martin@emerbee.com>
2017-06-05 15:01:38 +0200

    Got a workbench now

--- main.py
+++ main.py
@@ -3,4 +3,40 @@
 # source venv/bin/activate
 
 
-print "Hello, world"
+# from Tkinter import Tk, Toplevel, Canvas
+import math
+from scipy import misc, ndimage
+import numpy as np
+import skimage.morphology as mp
+import matplotlib.pyplot as plt
+
+plt.set_cmap('binary')
+
+def run(): 
+
+	a = np.invert(misc.imread("a.png")) # Image is black on white
+
+	# pc = point_cloud(a)
+	a_eroded = mp.thin(a)
+
+	plt.title('Original')
+	plt.imshow(a)
+	plt.figure()
+	plt.title('Thin')
+	plt.imshow(a_eroded)
+
+	plt.show()
+
+
+# image = np.zeros((max_x, max_y))
+# image[coordinates] = 1
+
+def point_cloud(a):
+	np.array(np.nonzero(a)).T
+
+
+
+
+# ------------------------------------------------------------------------
+
+run()
