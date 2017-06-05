d0861c7c6da00d995afc66f82e8ecd6b05e1ed8f
Martin Strandgren <martin@emerbee.com>
2017-06-05 16:23:01 +0200

    Found the neighbors

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 # from Tkinter import Tk, Toplevel, Canvas
 import math
-from scipy import misc, ndimage
+from scipy import misc, ndimage, spatial, odr
 import numpy as np
 import skimage.morphology as mp
 import matplotlib.pyplot as plt
@@ -16,15 +16,26 @@ def run():
 
 	a = np.invert(misc.imread("a.png")) # Image is black on white
 
-	# pc = point_cloud(a)
-	a_eroded = mp.thin(a)
+	pc = point_cloud(a)
+	kd_tree = spatial.KDTree(pc)
+	x = pc[np.random.randint(pc.shape[0]), :]
+	# neighbors_idx = kd_tree.query_ball_point(x, 5)
+	(distances, neighbors_idx) = kd_tree.query(x, 50)
+	neighbors = pc[neighbors_idx,:]
+	# x_sort = np.argsort(pc[0, :])
+	# y_sort = np.argsort(pc[1, :])
 
-	plt.title('Original')
+	data = np.concatenate(([x], neighbors))
+	beta = fitODR(data)
+	print(beta)
+
+	plt.scatter(neighbors[:,1], neighbors[:, 0], marker=".")
+	plt.scatter(x[1], x[0], marker="+")
+	# plt.scatter(pc[:,1], pc[:,0], marker="+")
+
+	plt.plot(np.array(range(0,100)) * beta[0] + beta[1])
+
 	plt.imshow(a)
-	plt.figure()
-	plt.title('Thin')
-	plt.imshow(a_eroded)
-
 	plt.show()
 
 
@@ -32,8 +43,17 @@ def run():
 # image[coordinates] = 1
 
 def point_cloud(a):
-	np.array(np.nonzero(a)).T
+	return np.array(np.nonzero(a)).T
 
+
+def fitODR(data):
+	def f(B, x):
+		return B[0]*x + B[1]
+	linear = odr.Model(f)
+	data = odr.Data(data[:,0], data[:,1])
+	o = odr.ODR(data, linear, beta0=[1,0])
+	out = o.run()
+	return out.beta
 
 
 
